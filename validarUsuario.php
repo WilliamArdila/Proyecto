081eeb897d720b48a471a10c6b1e1ac5a43061b7
Jorge Luis Canchón Espinosa <jlcanchon@misena.edu.co>
2017-01-03 16:38:39 -0500

    Update validarUsuario.php

--- validarUsuario.php
+++ validarUsuario.php
@@ -13,27 +13,30 @@ if(empty($usuario)||empty($password))
 ?>
 <?php 
 $jorge=conectar();
-$registro=$jorge->query("select Usuario,Contrasena from usuario where Usuario='" . $usuario . "'")
+$registro=$jorge->query("select Usuario,Contrasena,visibilidad from usuario where Usuario='" . $usuario . "'")
 or die($jorge->error);
 		if($registro==true)
 		{
 			$row=$registro->fetch_array();
-			if ($row['Usuario']==$usuario & $row['Contrasena']==$password) 
-			{
+				if($validar=password_verify($password, $row['Contrasena'])&$row['Usuario']==$usuario & $row['visibilidad']==1){
 				$j=$jorge->query("select empleado.Documento, empleado.NombreCompleto as Nombre from empleado inner join usuario on Usuario.Empleado_Documento=empleado.Documento where Usuario='" . $usuario . "'");
 				$jo=$j->fetch_array();
 				$_SESSION['usuario']=$jo['Nombre'];
-
+				$_SESSION['empleado']=$jo['Documento'];
 			    $_SESSION['estado']=1;
-				header("Location:index.php");
-			}else
+				header("Location:../controlers/Principal.php");
+				}
+			else
 			{
 				 $_SESSION['estado']=0;
-				 
-				header("Location:../publicidad/loguin.php");
+				  echo '<script type="text/javascript">	
+					alert("Usuario o contraseña incorrecta");
+					window.location="../publicidad/loguin.php";
+					</script>'; 
 			}
 		}else
 		{
+			$_SESSION['estado']=0;
 			header("Location:../publicidad/loguin.php");
 		}
   ?>
